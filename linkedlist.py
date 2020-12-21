323b2388ba04498dd0cf65eeda0e9a14950f4db0
tynski <b.tynski@gmail.com>
2020-12-21 20:04:11 +0100

    double linked list

--- linkedlist.py
+++ linkedlist.py
@@ -2,6 +2,7 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+        self.previous = None
     
     def __repr__(self):
         return self.data
@@ -12,9 +13,13 @@ class LinkedList:
         if nodes is not None:
             node = Node(data=nodes.pop(0))
             self.head = node
+            prev_node = self.head
             for elem in nodes:
                 node.next = Node(elem)
                 node = node.next
+                node.previous = prev_node
+                prev_node = node
+
 
     def __iter__(self):
         node = self.head
@@ -32,6 +37,7 @@ class LinkedList:
         return " -> ".join(nodes)
 
     def add_first(self, node):
+        self.head.previous = node
         node.next = self.head
         self.head = node
 
@@ -42,6 +48,7 @@ class LinkedList:
         for current_node in self:
             pass
         current_node.next = node
+        node.previous = current_node
 
     def add_after(self, target_node_data, new_node):
         if not self.head:
@@ -50,7 +57,9 @@ class LinkedList:
         for node in self:
             if node.data == target_node_data:
                 new_node.next = node.next
+                node.next.previous = new_node
                 node.next = new_node
+                new_node.previous = node
                 return
         
         raise Exception("Node with data %s not found!" % target_node_data)
@@ -67,6 +76,8 @@ class LinkedList:
         for node in self:
             if node.data == target_node_data:
                 prev_node.next = new_node
+                new_node.previous = prev_node
+                node.previous = new_node
                 new_node.next = node
                 return
             prev_node = node
@@ -79,12 +90,14 @@ class LinkedList:
 
         if self.head == target_node_data:
             self.head = self.head.next
+            self.head.previous = None
             return
 
         prev_node = self.head
         for node in self:
             if node.data == target_node_data:
-                prev_node.next = node.next 
+                prev_node.next = node.next
+                node.next.previous = prev_node
                 return    
             prev_node = node
 
@@ -99,11 +112,9 @@ llist.add_last(Node("e"))
 llist.add_last(Node("f"))
 
 llist.add_after("2", Node("1.4"))
-#llist.add_after("10", Node("11"))
 
 llist.add_before("e", Node("d"))
-#llist.add_before("z", Node("x"))
 llist.remove_node("a")
 
 for node in llist:
-    print(node)
+    print("prev: {}, current: {}, next: {}\n".format("None" if not node.previous else node.previous.data, node.data, "None" if not node.next else node.next.data))
