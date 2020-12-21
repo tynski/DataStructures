06f4f646211292e7c869c22d4df0c7ae8c034226
tynski <b.tynski@gmail.com>
2020-12-21 20:05:19 +0100

    refactoring

--- linkedlist.py
+++ linkedlist.py
@@ -3,9 +3,10 @@ class Node:
         self.data = data
         self.next = None
         self.previous = None
-    
+
     def __repr__(self):
         return self.data
+
 
 class LinkedList:
     def __init__(self, nodes):
@@ -19,7 +20,6 @@ class LinkedList:
                 node = node.next
                 node.previous = prev_node
                 prev_node = node
-
 
     def __iter__(self):
         node = self.head
@@ -53,7 +53,7 @@ class LinkedList:
     def add_after(self, target_node_data, new_node):
         if not self.head:
             raise Exception("List is empty!")
-        
+
         for node in self:
             if node.data == target_node_data:
                 new_node.next = node.next
@@ -61,17 +61,17 @@ class LinkedList:
                 node.next = new_node
                 new_node.previous = node
                 return
-        
+
         raise Exception("Node with data %s not found!" % target_node_data)
 
     def add_before(self, target_node_data, new_node):
         if not self.head:
-            raise Exception("List is empty!")     
-        
+            raise Exception("List is empty!")
+
         if self.head.data == target_node_data:
             self.add_first(new_node)
             return
-        
+
         prev_node = self.head
         for node in self:
             if node.data == target_node_data:
@@ -81,9 +81,9 @@ class LinkedList:
                 new_node.next = node
                 return
             prev_node = node
-        
+
         raise Exception("Node with data %s not found!" % target_node_data)
-    
+
     def remove_node(self, target_node_data):
         if not self.head:
             raise Exception("List is empty!")
@@ -98,10 +98,11 @@ class LinkedList:
             if node.data == target_node_data:
                 prev_node.next = node.next
                 node.next.previous = prev_node
-                return    
+                return
             prev_node = node
 
         raise Exception("Node with data %s not found!" % target_node_data)
+
 
 llist = LinkedList(["a", "b", "c"])
 
@@ -117,4 +118,5 @@ llist.add_before("e", Node("d"))
 llist.remove_node("a")
 
 for node in llist:
-    print("prev: {}, current: {}, next: {}\n".format("None" if not node.previous else node.previous.data, node.data, "None" if not node.next else node.next.data))
+    print("prev: {}, current: {}, next: {}".format(
+        "None" if not node.previous else node.previous.data, node.data, "None" if not node.next else node.next.data))
